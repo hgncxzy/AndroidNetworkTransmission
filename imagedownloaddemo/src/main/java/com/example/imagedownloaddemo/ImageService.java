7da34aaf18786f849d720ca0db4d403ebcbb3c62
许助云 <xuzhuyun@fcbox.com>
2019-07-13 17:14:10 +0800

    feat:  添加图片下载 demo。

---
+++ imagedownloaddemo/src/main/java/com/example/imagedownloaddemo/ImageService.java
@@ -0,0 +1,31 @@
+package com.example.imagedownloaddemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 图片下载工具类封装。
+ */
+class ImageService {
+
+    /**
+     * 获取图片
+     * @param path 图片路径
+     * @return bitmap
+     */
+    static Bitmap getImage(String path) throws Exception{
+        URL url = new URL(path);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(5000);
+        conn.setRequestMethod("GET");
+        if(conn.getResponseCode() == 200){
+            InputStream inStream = conn.getInputStream();
+            return BitmapFactory.decodeStream(inStream);
+        }
+        return null;
+    }
+}
