fd93f857763cbec864c9e1e8e9ff0c4d03ce5268
hgncxzy <hgncxzy@qq.com>
2019-09-02 23:29:16 +0800

    补充提交

--- filedownload/src/main/java/com/example/filedownload/MainActivity.java
+++ filedownload/src/main/java/com/example/filedownload/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.filedownload;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
